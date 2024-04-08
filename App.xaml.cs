d0295c6622bfba5a86dec27dbbe09af36c0fc91c
Nantha Kishor <nantha.kishor@amcsgroup.com>
2024-04-08 11:48:55 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ZXingMauiQrBarcodeScanner
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using ZXing.Net.Maui;
+
+namespace ZXingMauiQrBarcodeScanner
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+			barcodeReader.Options = new BarcodeReaderOptions
+			{
+				Formats = BarcodeFormats.All,
+				AutoRotate = true,
+				Multiple = false
+			};
+			barcodeReader.CameraLocation = CameraLocation.Front;
+		}
+
+		public void BarcodesDetected(object sender, BarcodeDetectionEventArgs e)
+		{
+			var first = e.Results?.FirstOrDefault();
+			if (first is null)
+				return;
+
+			Dispatcher.DispatchAsync(async () =>
+			{
+				await DisplayAlert("Barcode Detected", first.Value, "OK");
+			});
+		}
+
+	}
+}
